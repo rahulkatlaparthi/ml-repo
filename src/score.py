1b2d43df26fb9b0049fb56dbe69ecc69616d7540
rahulkatlaparthi <69039932+rahulkatlaparthi@users.noreply.github.com>
2025-11-05 11:02:55 +0530

    Update score.py

--- src/score.py
+++ src/score.py
@@ -5,7 +5,7 @@ import os
 
 def init():
     global model
-    model_path = os.path.join(os.getenv("AZUREML_MODEL_DIR"), "model.pkl")
+    model_path = os.path.join(os.getenv("AZUREML_MODEL_DIR"), "model.joblib")
     model = joblib.load(model_path)
 
 def run(raw_data):
